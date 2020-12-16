4bf7e159ba97b9c750a7c7c1ebed3c1e7c8e0404
Samuel Marandi <samuel@91social.com>
2020-12-16 10:42:49 +0530

    update service urls

--- Moviebook_UI/src/common/services/moviebook.service.js
+++ Moviebook_UI/src/common/services/moviebook.service.js
@@ -1,10 +1,12 @@
 import config from "../config";
 import axios from "axios";
 
+// TODO: Remove mocky once confirmed
 const getMovieDetails = async (token, keywords) => {
 	const url = `${config.apiBaseUrl}/listmovies?filmName=${keywords}`;
 
 	const headers = {
+		"Content-Type": "application/json",
 		Authorization: `${token}`,
 	};
 
@@ -12,9 +14,11 @@ const getMovieDetails = async (token, keywords) => {
 };
 
 const fetchAllCategoryGroups = async (token) => {
-	const url = "https://run.mocky.io/v3/59b51440-bcc0-41b6-a3e5-223336dbd062";
+	const url = `${config.apiBaseUrl}/genre-groups`,
+	// const url = "https://run.mocky.io/v3/59b51440-bcc0-41b6-a3e5-223336dbd062";
 
 	const headers = {
+		"Content-Type": "application/json",
 		Authorization: `${token}`,
 	};
 
@@ -22,11 +26,12 @@ const fetchAllCategoryGroups = async (token) => {
 };
 
 const fetchMovieListByCategoryGroup = async (token, genreName) => {
-	// const url = `${config.apiBaseUrl}/getMovieList?genreName=${genreName}`;
+	const url = `${config.apiBaseUrl}/getMovieList?genreName=${genreName}`;
 
-	const url = `https://run.mocky.io/v3/0afce8e2-4f08-4a91-80c1-8b4aead88151?genreName=${genreName}`;
+	// const url = `https://run.mocky.io/v3/0afce8e2-4f08-4a91-80c1-8b4aead88151?genreName=${genreName}`;
 
 	const headers = {
+		"Content-Type": "application/json",
 		Authorization: `${token}`,
 	};
 
@@ -34,10 +39,11 @@ const fetchMovieListByCategoryGroup = async (token, genreName) => {
 };
 
 const fetchMovieList = async (token) => {
-	//   const url = `${config.apiBaseUrl}/getMovieList`;
-	const url = "https://run.mocky.io/v3/0afce8e2-4f08-4a91-80c1-8b4aead88151";
+	const url = `${config.apiBaseUrl}/getMovieList`;
+	// const url = "https://run.mocky.io/v3/0afce8e2-4f08-4a91-80c1-8b4aead88151";
 
 	const headers = {
+		"Content-Type": "application/json",
 		Authorization: `${token}`,
 	};
 
@@ -45,10 +51,11 @@ const fetchMovieList = async (token) => {
 };
 
 const fetchAllCategories = async (token) => {
-	//   const url = `${config.apiBaseUrl}/categories'`;
-	const url = "https://run.mocky.io/v3/7d0d59dc-a002-401a-a038-241ed02c8a96";
+	const url = `${config.apiBaseUrl}/categories'`;
+	// const url = "https://run.mocky.io/v3/7d0d59dc-a002-401a-a038-241ed02c8a96";
 
 	const headers = {
+		"Content-Type": "application/json",
 		Authorization: `${token}`,
 	};
 
@@ -56,7 +63,8 @@ const fetchAllCategories = async (token) => {
 };
 
 const createNewCategory = async (token, newCategoryGroup) => {
-	const url = "https://run.mocky.io/v3/8daff231-11ee-4105-ab15-5ca0e4578f00";
+	const url = `${config.apiBaseUrl}/genre-group`;
+	// const url = "https://run.mocky.io/v3/8daff231-11ee-4105-ab15-5ca0e4578f00";
 
 	const headers = {
 		"Content-Type": "application/json",
@@ -67,11 +75,10 @@ const createNewCategory = async (token, newCategoryGroup) => {
 };
 
 const deleteCategoryGroup = async (token, genreGroupId) => {
-	// http://localhost:9999/MIRest_war/minfo/delete-genre-group/2'
-	const url = "https://run.mocky.io/v3/b2624689-2649-4adf-8934-83ab2a66a239";
+	const url = `${config.apiBaseUrl}/delete-genre-group/${genreGroupId}`
+	// const url = "https://run.mocky.io/v3/b2624689-2649-4adf-8934-83ab2a66a239";
 
 	const headers = {
-		"Content-Type": "application/json",
 		Authorization: `${token}`,
 	};
 
