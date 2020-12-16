68a73f8bb6482b39f250d47e3213769e6816124b
Samuel Marandi <samuel@91social.com>
2020-12-16 07:50:59 +0530

    corrected back config

--- Moviebook_UI/src/common/config.js
+++ Moviebook_UI/src/common/config.js
@@ -1,7 +1,5 @@
 const config = {
-  // apiBaseUrl: "/minfo",
-  apiBaseUrl: "https://moviebook.dev.cubecorp.io/minfo",
-  // apiBaseUrl: "http://localhost:9001/movieinfo/minfo",
+  apiBaseUrl: "/minfo",
 };
 
 export default config;
