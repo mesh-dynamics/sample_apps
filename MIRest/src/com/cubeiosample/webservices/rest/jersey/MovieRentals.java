094bf6bf4b61a3cfa6d44c653b51cafe89b6c7de
Gaurav Kulshreshtha <gaurav.kulshreshtha@meshdynamics.io>
2021-01-18 13:03:28 +0530

    one more egress inventory check call from rent movie api

--- MIRest/src/com/cubeiosample/webservices/rest/jersey/MovieRentals.java
+++ MIRest/src/com/cubeiosample/webservices/rest/jersey/MovieRentals.java
@@ -333,6 +333,10 @@ public class MovieRentals {
     	}
     	result.put("rent", rentalRate*duration);
     	LOGGER.debug("rent movie result returning: " + result.toString());
+    	// creating a test case when one ingress call creates two (same) egress call but with different response.
+	    // mock should be order sensitive and return 1st egress response for 1st egress call and 2nd response for 2nd egress call.
+	    inventoryId = getInventoryId(film_id, store_id);
+	    LOGGER.debug("second inventory Id "+inventoryId);
     	return result;
     }
 
